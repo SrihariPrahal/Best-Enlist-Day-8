83b0b111137886da3d66d708531599c700823f86
SrihariPrahal <75318418+SrihariPrahal@users.noreply.github.com>
2020-12-04 12:17:39 +0530

    Add files via upload

---
+++ removeObject.js
@@ -0,0 +1,9 @@
+Array.prototype.remove = function removeObject(){
+    obj.remove({'address':'xyz'});
+    
+}
+var obj = [{'id':1,'age':24,'address':'xyz'},{'id':2,'age':27,'address':'xyz'}];
+return removeObject();
+
+
+
